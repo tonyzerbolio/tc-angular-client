c00270a3d9466302a9abec06b57c1a53eeea8269
tzerbass <tzer.bass@gmail.com>
2019-05-01 11:44:04 -0500

    fixi folder name errors feature-module conversion

--- src/app/customers-list/customer-list.module.ts
+++ src/app/customers-list/customer-list.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { CustomersListComponent } from './customer-list/customers-list.component';
+import { CustomersListComponent } from './customers-list/customers-list.component';
 
 
 @NgModule({
