8d1e396cc0d15934749c198ef1ef1451629ee808
tzerbass <tzer.bass@gmail.com>
2019-05-03 11:37:02 -0500

    Fixed linting issues

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -18,8 +18,7 @@
  * BROWSER POLYFILLS
  */
 
- 
-/** Evergreen browsers require these. **/
+/** Evergreen browsers require these. */
 import 'core-js/es6/reflect';
 import 'core-js/es7/reflect';
 import 'core-js/client/shim';
@@ -72,4 +71,4 @@ import 'zone.js/dist/zone';  // Included with Angular CLI.
  * Date, currency, decimal and percent pipes.
  * Needed for: All but Chrome, Firefox, Edge, IE11 and Safari 10
  */
- import 'intl';  // Run `npm install --save intl`.
+import 'intl';  // Run `npm install --save intl`.
